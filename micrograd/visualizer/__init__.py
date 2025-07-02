26542500d606d620de590c3069aadd45ac9771ab
striderarun <strider90arun@gmail.com>
2025-07-02 10:09:13 -0700

    Micrograd visualizer

---
+++ micrograd/visualizer/__init__.py
@@ -0,0 +1,5 @@
+# Create files in this directory as a regular package.
+# Allows py files in this directory to refer to py files in parent directories (assuming they also are defined as regular package)
+# https://stackoverflow.com/questions/37139786/is-init-py-not-required-for-packages-in-python-3-3
+
+# Call graph.py as python -m visualizer.graph from root.
---
+++ micrograd/visualizer/graph.py
@@ -0,0 +1,46 @@
+from graphviz import Digraph
+from engine import Value
+
+def trace(root):
+    nodes, edges = set(), set()
+    def build(v):
+        if v not in nodes:
+            nodes.add(v)
+            for child in v._prev:
+                edges.add((child, v))
+                build(child)
+    build(root)
+    return nodes, edges
+
+def draw_dot(root, format='svg', rankdir='LR'):
+    """
+    format: png | svg | ...
+    rankdir: TB (top to bottom graph) | LR (left to right)
+    """
+    assert rankdir in ['LR', 'TB']
+    nodes, edges = trace(root)
+    dot = Digraph(format=format, graph_attr={'rankdir': rankdir}) #, node_attr={'rankdir': 'TB'})
+    
+    for n in nodes:
+        dot.node(name=str(id(n)), label = "{ %s | data %.4f | grad %.4f}" % (n.label, n.data, n.grad), shape='record')
+        if n._op:
+            dot.node(name=str(id(n)) + n._op, label=n._op)
+            dot.edge(str(id(n)) + n._op, str(id(n)))
+    
+    for n1, n2 in edges:
+        dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+    
+    return dot
+
+# Call as python -m visualizer.graph
+if __name__ == '__main__':
+    a = Value(2.0, label='a')
+    b = Value(-3.0, label='b')
+    c = Value(10.0, label='c')
+    d = a*b; d.label = 'd'
+    e = d + c; e.label='e'
+    f = Value(-2.0, label='f')
+    L = e * f; L.label='L'
+
+    graph = draw_dot(L)
+    graph.view()
